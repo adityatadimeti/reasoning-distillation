8247d777884dd64115db089274543389c255f306
Vivek Vajipey <vivek.vajipey@gmail.com>
2025-04-07 13:07:59 -0700

    llama 70b

--- gpqa_experiments.sh
+++ gpqa_experiments.sh
@@ -1,4 +1,6 @@
-python run_experiment.py gpqa_deepseek_qwen_1_5b_diff_llama4_sum --parallel --concurrency 32
-python run_experiment.py gpqa_deepseek_qwen_14b_diff_llama4_sum --parallel --concurrency 32
-python run_experiment.py gpqa_deepseek_qwen_14b_diff_dsv3_sum --parallel --concurrency 32
-python run_experiment.py gpqa_deepseek_qwen_70b_diff_llama4_sum --parallel --concurrency 32
+# python run_experiment.py gpqa_deepseek_qwen_1_5b_diff_llama4_sum --parallel --concurrency 32
+# python run_experiment.py gpqa_deepseek_qwen_7b_diff_llama4_sum --parallel --concurrency 32
+# python run_experiment.py gpqa_deepseek_qwen_14b_diff_llama4_sum --parallel --concurrency 32
+
+# python run_experiment.py gpqa_deepseek_qwen_14b_diff_dsv3_sum --parallel --concurrency 32
+# python run_experiment.py gpqa_deepseek_llama_70b_diff_llama4_sum --parallel --concurrency 32
