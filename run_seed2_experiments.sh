e20e0263bfa83ace478c80588c3455347da61b5f
Vivek Vajipey <vivek.vajipey@gmail.com>
2025-05-13 22:49:03 -0700

    changed orders

--- run_seed2_experiments.sh
+++ run_seed2_experiments.sh
@@ -2,12 +2,13 @@
 
 echo "Running Seed 2 experiments..."
 
-# Post think
-python run_experiment.py aime_deepseek_qwen_14b_baseline_post_think_sum_4iter_seed_2 --parallel --concurrency 12 \
-  --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2_20250513_002335/results.json
-
 # Base summary
 python run_experiment.py aime_deepseek_qwen_14b_summ_base_sum_4iter_seed_2 --parallel --concurrency 12 \
+  --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2_20250513_002335/results.json
+
+
+# Post think
+python run_experiment.py aime_deepseek_qwen_14b_baseline_post_think_sum_4iter_seed_2 --parallel --concurrency 12 \
   --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_2_20250513_002335/results.json
 
 # First k
--- run_seed3_experiments.sh
+++ run_seed3_experiments.sh
@@ -2,6 +2,11 @@
 
 echo "Running Seed 3 experiments..."
 
+# First k
+python run_experiment.py aime_deepseek_qwen_14b_baseline_firstk_sum_4iter_seed_3 --parallel --concurrency 12 \
+  --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3_20250513_110758/results.json
+
+
 # Post think
 python run_experiment.py aime_deepseek_qwen_14b_baseline_post_think_sum_4iter_seed_3 --parallel --concurrency 12 \
   --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3_20250513_110758/results.json
@@ -12,10 +17,6 @@ python run_experiment.py aime_deepseek_qwen_14b_summ_base_sum_4iter_seed_3 --par
 
 # Last k
 python run_experiment.py aime_deepseek_qwen_14b_baseline_lastk_sum_4iter_seed_3 --parallel --concurrency 12 \
-  --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3_20250513_110758/results.json
-
-# First k
-python run_experiment.py aime_deepseek_qwen_14b_baseline_firstk_sum_4iter_seed_3 --parallel --concurrency 12 \
   --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_3_20250513_110758/results.json
 
 echo "Seed 3 experiments completed!" 
