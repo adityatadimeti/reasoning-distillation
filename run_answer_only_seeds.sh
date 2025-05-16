642babe58a7d6eed8c4d75d83a2f239073c3d6e7
Vivek Vajipey <vivek.vajipey@gmail.com>
2025-05-16 12:19:09 -0400

    remaining answer only

--- run_answer_only_seeds.sh
+++ run_answer_only_seeds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# seed 1
-python run_experiment.py aime_deepseek_qwen_14b_baseline_answer_only_sum_4iter_seed_1 --parallel --concurrency 8 \
-  --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_1/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_1_20250512_202741/merged_results_20250513_220116.json
+# # seed 1
+# python run_experiment.py aime_deepseek_qwen_14b_baseline_answer_only_sum_4iter_seed_1 --parallel --concurrency 8 \
+#   --load_initial_reasoning=results/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_1/aime_deepseek_qwen_14b_summ_base_sum_4iter_backtracking_seed_1_20250512_202741/merged_results_20250513_220116.json
 
 # seed 2
 python run_experiment.py aime_deepseek_qwen_14b_baseline_answer_only_sum_4iter_seed_2 --parallel --concurrency 8 \
-  --load_initial_reasoning=results/aime_deepseek_qwen_14b_baseline_lastk_sum_4iter_seed_2/aime_deepseek_qwen_14b_baseline_lastk_sum_4iter_seed_2_20250513_121629/results.json
+  --load_initial_reasoning=results/aime_deepseek_qwen_14b_baseline_lastk_sum_4iter_seed_2/aime_deepseek_qwen_14b_baseline_lastk_sum_4iter_seed_2_20250513_121629/results.json \
+  --question_ids=14,16,5,28,9,19,18,29,2,20,24,15,11,22,17,27,21,23,10,26,13,12,25
 
 # seed 3
 python run_experiment.py aime_deepseek_qwen_14b_baseline_answer_only_sum_4iter_seed_3 --parallel --concurrency 8 \
